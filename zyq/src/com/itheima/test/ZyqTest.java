7ea945917ecef9651e66e58fa7376671acde686f
zhengyq <3235127443@qq.com>
2020-11-03 17:01:21 +0800

    把本地工程添加到git1里

---
+++ zyq/src/com/itheima/test/ZyqTest.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class ZyqTest {
+    public static void main(String[] args) {
+        System.out.println("git使用了.....");
+        System.out.println("git使用了.....");
+        System.out.println("git使用了.....");
+        System.out.println("git使用了.....");
+        System.out.println("git使用了.....");
+        System.out.println("git使用了.....");
+    }
+}
